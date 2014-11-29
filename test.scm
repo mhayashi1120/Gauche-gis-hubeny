363bf83c2b953a2fd2b27997fa10531188be4cb4
Masahiro Hayashi <mhayashi1120@gmail.com>
2014-11-29 20:02:47 +0900

    read negative longitude/latitude (add testcase) reduce calculaate error limit

--- test.scm
+++ test.scm
@@ -8,9 +8,9 @@
 (use gis.hubeny)
 (test-module 'gis.hubeny)
 
-;; allow 0.1% error
+;; allow 0.5% error
 (define-method nearly=? ((f1 <real>) (f2 <real>))
-  (<= (* 0.999 f2) f1 (* 1.001 f2)))
+  (<= (* 0.995 f2) f1 (* 1.005 f2)))
 
 (let* ([Tokyo "35.65500,139.74472"]
        [Tsukuba "36.10056,140.09111"]
@@ -19,7 +19,12 @@
   (test* "Distance from Yamadarake1"
          58501.873 (distance-between Tokyo Tsukuba) nearly=?)
   (test* "Distance from Yamadarake2"
-         889826.431 (distance-between Tokyo FukuokaDome) nearly=?))
+         889826.431 (distance-between Tokyo FukuokaDome) nearly=?)
+  (test* "Distance from Google Map"
+         261690.0 (distance-between "51.492552, 0.237717" "51.293807, -3.520984") nearly=?)
+  (test* "Distance from Google Map 2"
+         2255100.0 (distance-between "4.741940, 15.557547" "-11.450795, 3.284581") nearly=?)
+  )
   
 ;; If you don't want `gosh' to exit with nonzero status even if
 ;; the test fails, pass #f to :exit-on-failure.
